a52fc9def98c4998f951d7f638b0f27ca292bf64
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-01-11 14:17:48 +0400

    without dependency injection

---
+++ src/main/java/com/springboot/dependencyInjection/services/GreetingServiceImpl.java
@@ -0,0 +1,11 @@
+package com.springboot.dependencyInjection.services;
+
+public class GreetingServiceImpl implements GrettingService {
+
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return "Hello Everyone from base service.";
+	}
+
+}
