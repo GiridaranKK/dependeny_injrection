bf25f07f4d5eb0596312083e7e0572fd827eb450
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-01-19 22:15:23 +0400

    Default profiles on spring

--- src/main/java/com/springboot/dependencyInjection/services/EnglishGreetingService.java
+++ src/main/java/com/springboot/dependencyInjection/services/EnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.springboot.dependencyInjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GrettingService{
 
